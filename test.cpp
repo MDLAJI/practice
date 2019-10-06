86b5f44a55a9537cc8b3ea0bb4bdb4acac9d4974
MDLAJI <1844120008@qq.com>
2019-10-06 11:56:48 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -2,6 +2,11 @@
 using namespace std;
 int main()
 {
+<<<<<<< HEAD:test.cpp
 	cout<<"hello worid";
 	return 0;
+=======
+  cout<<"Hello Worid";
+  return 0;
+>>>>>>> parent of 5f3a6f5... 修改:Hello_Worid.cpp
 }
